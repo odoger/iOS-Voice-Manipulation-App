3057aad0512d6982b029572d4a2286044925b4cc
Oğuz Döğer <OGUZ@mb-oguz.local>
2017-11-21 20:10:25 +0300

    Initial Commit

---
+++ Voice Manipulation App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Voice Manipulation App
+//
+//  Created by Oğuz Döğer on 21.11.2017.
+//  Copyright © 2017 Oguz Doger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
